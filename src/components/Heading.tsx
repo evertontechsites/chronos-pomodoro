12ce29246ae429610b7dda34d3071ea814fa6a10
Everton Ferreira <everton,tech.sites@gmail.com>
2026-04-06 00:44:41 -0300

    Finalização do componente Heading e tipagem de children(React.ReactNode)

---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import style from './Heading.module.css';
+
+type HeadingProps = {
+    children: React.ReactNode;
+}
+
+export function Heading({ children }: HeadingProps) {
+    return <h1 className={style.heading} >{children}</h1>
+}
